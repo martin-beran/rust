49770c47ea4d25e19e0342266bd0cb8097323cb2
Martin Beran <martin.beran@avast.com>
2019-08-20 23:42:19 +0200

    experiment: writes arguments and environment

--- experiment/src/main.rs
+++ experiment/src/main.rs
@@ -1,6 +1,22 @@
-use std::env
+use std::env;
 
 fn main() {
     let args = env::args();
+    let mut i = 0;
+    for a in args {
+        arg(i, a);
+        i += 1;
+    }
+    for e in env::vars() {
+        let (n, v) = e;
+        env(n, v);
+    }
+}
 
+fn arg(i: i32, v: String) {
+    println!("argv[{}] = {}", i, v);
+}
+
+fn env(n: String, v: String) {
+    println!("{}={}", n, v);
 }
--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    let x = 5;
+    let r = &x;
+    println!("x={} r={}", x, r);
+    let x = 6;
+    println!("x={} r={}", x, r);
+
+    let t = (1, 2, 3, 4);
+    println!("t2={}", t.2);
 }
