1abe47a9e9727dd1ced3695fb5198086d7892df9
Martin Beran <martin.beran@avast.com>
2019-08-21 18:38:45 +0200

    split into functions

--- workshop/src/main.rs
+++ workshop/src/main.rs
@@ -5,26 +5,12 @@ use std::vec::Vec;
 use walkdir::Error;
 use walkdir::WalkDir;
 
+type FileHash = HashMap<md5::Digest, Vec<PathBuf>>;
+
 fn main() -> Result<(), Error> {
     let files = get_files()?;
-    let mut hash: HashMap<md5::Digest, Vec<PathBuf>> = HashMap::new();
-    for f in files {
-        match std::fs::read(&f) {
-            Ok(data) => {
-                let digest = md5::compute(data);
-                hash.entry(digest).or_default().push(f);
-            },
-            Err(_) => {},
-        }
-    }
-    for (_digest, eq_files) in hash.iter() {
-        if eq_files.len() >= 2 {
-            println!("{}", eq_files[0].display());
-            for i in 1..(eq_files.len()) {
-                println!("    = {}", eq_files[i].display());
-            }
-        }
-    }
+    let hash = find_dupl(files);
+    show_dupl(&hash);
 
     return Ok(());
 }
@@ -35,4 +21,29 @@ fn get_files() -> Result<Vec<PathBuf>, Error> {
         files.push(f?.path().to_path_buf());
     }
     return Result::Ok(files);
+}
+
+fn find_dupl(files: Vec<PathBuf>) -> FileHash {
+    let mut hash = FileHash::new();
+    for f in files {
+        match std::fs::read(&f) {
+            Ok(data) => {
+                let digest = md5::compute(data);
+                hash.entry(digest).or_default().push(f);
+            },
+            Err(_) => {},
+        }
+    }
+    return hash;
+}
+
+fn show_dupl(hash: &FileHash) {
+    for (_digest, eq_files) in hash.iter() {
+        if eq_files.len() >= 2 {
+            println!("{}", eq_files[0].display());
+            for i in 1..(eq_files.len()) {
+                println!("    = {}", eq_files[i].display());
+            }
+        }
+    }
 }
