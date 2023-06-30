a2d159bc8b92f941e9152ac4c352006447557ed9
Martin Beran <martin@mber.cz>
2023-07-01 00:49:36 +0200

    Using Result

--- panic/src/main.rs
+++ panic/src/main.rs
@@ -3,9 +3,12 @@ fn main() {
     for a in std::env::args() {
         args.push(a);
     }
-    do_panic(&args[1]);
+    open_file(&args[1]);
 }
 
-fn do_panic(arg1: &str) {
-    panic!("arg={arg1}");
+fn open_file(arg1: &str) {
+    let _fh = match std::fs::File::open(arg1) {
+        Ok(fh) => fh,
+        Err(e) => panic!("Cannot open file: {:?}", e),
+    };
 }
