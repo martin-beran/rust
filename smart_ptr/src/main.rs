b14e550b1a90aff706bb34b5ca83fa9b1481cc6e
Martin Beran <martin@mber.cz>
2023-07-05 23:01:11 +0200

    Improved progral smart_ptr

--- smart_ptr/src/main.rs
+++ smart_ptr/src/main.rs
@@ -19,8 +19,21 @@ impl BoxList<String> {
     }
 }
 
+struct MyBox<T>(T);
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+impl<T> std::ops::Deref for MyBox<T> {
+    type Target = T;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
 fn main() {
-    let list = BoxList::Node(String::from("Hello"),
+    let mut list = BoxList::Node(String::from("Hello"),
         Box::new(BoxList::Node(String::from(" "),
             Box::new(BoxList::Node(String::from("world"),
                 Box::new(BoxList::Node(String::from("!"),
@@ -30,4 +43,26 @@ fn main() {
         ))
     );
     println!("list={}", list.to_string());
+    let mut p = &list;
+    for _ in 0..2 {
+        match p {
+            BoxList::Null => {},
+            BoxList::Node(_, pp) => p = pp,
+        }
+    }
+    println!("p={}", p.to_string());
+    let mut p = &mut list;
+    for _ in 0..3 {
+        match p {
+            BoxList::Null => {},
+            BoxList::Node(_, pp) => p = pp,
+        }
+    }
+    *p = BoxList::Null;
+    println!("list={}", list.to_string());
+
+    let x = 5;
+    let y = MyBox::new(x);
+    assert!(x == 5);
+    assert!(*y == 5);
 }
