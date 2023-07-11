ee83d0ace50b3745d9fc54715d770d4592113a8d
Martin Beran <martin@mber.cz>
2023-07-12 00:26:16 +0200

    Combining traits

--- trait_object/src/main.rs
+++ trait_object/src/main.rs
@@ -4,6 +4,14 @@ trait Shape {
     }
 }
 
+trait Num {
+    fn val(&self) -> f64 {
+        0.0
+    }
+}
+
+trait NumShape: Shape + Num {}
+
 struct Point {
     x: f64,
     y: f64,
@@ -12,6 +20,13 @@ impl Shape for Point {
     fn draw(&self) {
         println!("Point x={} y={}", self.x, self.y);
     }
+}
+impl Num for Point {
+    fn val(&self) -> f64 {
+        self.x + self.y
+    }
+}
+impl NumShape for Point {
 }
 
 struct Circle {
@@ -24,17 +39,24 @@ impl Shape for Circle {
         println!("Circle x={} y={} r={}", self.x, self.y, self.r);
     }
 }
+impl Num for Circle {
+    fn val(&self) -> f64 {
+        self.x + self.y + self.r
+    }
+}
+impl NumShape for Circle {
+}
 
-fn draw_shape(s: &dyn Shape) {
+fn draw_shape(s: &dyn NumShape) {
     s.draw();
 }
 
 fn main() {
     let p = Point{x: 1.2, y:2.3};
     let c = Circle{x: -1.2, y: -2.3, r: 4.5};
-    let v: Vec<Box<dyn Shape>> = vec![Box::new(p), Box::new(c)];
+    let v: Vec<Box<dyn NumShape>> = vec![Box::new(p), Box::new(c)];
     for s in v {
         draw_shape(&*s);
-        s.draw();
+        println!("val={}", s.val());
     }
 }
