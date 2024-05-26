df65075107ea58331fd18b692a0920c7e3cd56c1
Martin Beran <martin@mber.cz>
2024-05-27 01:19:05 +0200

    WiP: Program expr - implementation of expr_cpp in Rust

--- expr/src/main.rs
+++ expr/src/main.rs
@@ -1,3 +1,47 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::process::{ExitCode, Termination};
+
+fn main() -> impl Termination {
+    let mut argv = env::args();
+    let argv0 = argv.next().unwrap();
+    if argv.len() != 1 {
+        return usage(&argv0);
+    }
+    let argv1 = argv.next().unwrap();
+    match argv1.as_str() {
+        "i" => run::<i32>(),
+        "u" => run::<u32>(),
+        "d" => run::<f64>(),
+        "s" => run::<&str>(),
+        _ => usage(&argv0),
+    }
+}
+
+fn usage(argv0: &str) -> ExitCode {
+    eprintln!("usage: {} {}", argv0, "{i|u|d|s}
+
+Evaluates an expression read from stdin consisting of:
+- values
+- unary operator -
+- binary operators +, -, *, /
+- parentheses
+- whitespace (ignored)
+
+Selection of type:
+
+i = i32
+u = u32
+d = f64
+s = str (only binary +, whitespace needed around operators and parentheses)
+");
+    ExitCode::FAILURE
+}
+
+fn run<T>() -> ExitCode {
+    ExitCode::SUCCESS
+}
+
+trait Expr<T> {
+    fn eval(&self) -> Option<T>;
+    fn display(&self);
 }
